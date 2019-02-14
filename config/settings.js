b1ceb551260b0f7a0e381b562f12d0695409a7db
Anthony Velito <anthon.velito@sap.com>
2019-02-14 15:25:51 -0500

    adding sap cloud configuration files

--- config/settings.js
+++ config/settings.js
@@ -2,7 +2,7 @@ const path     = require('path');
 
 const settings = {
   path    : path.normalize(path.join(__dirname, '..')),
-  port    : process.env.NODE_PORT || 3000
+  port    : process.env.PORT || 8088
 };
 
 module.exports = settings;
